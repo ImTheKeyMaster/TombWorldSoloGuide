9f44750f29541f99bfca3975c00b0d47b30966f0
ImTheKeyMaster <jrbenning@hotmail.com>
2026-07-20 10:04:28 -0400

    Fix duplicate v3.5.1 version references (#22)

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const APP_VERSION = '3.5.1';
-const APP_VERSION = '3.4.4';
 const CACHE_PREFIX = 'tomb-world-solo-guide-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const APP_SHELL = './index.html';
