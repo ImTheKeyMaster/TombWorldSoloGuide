f461876b7956e86c93bb32266dc1a8ba3c0d5492
ImTheKeyMaster <jrbenning@hotmail.com>
2026-07-21 21:03:36 -0400

    Simplify deployment roster cards

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const APP_VERSION = '5.7.0';
+const APP_VERSION = '5.7.1';
 const CACHE_PREFIX = 'tomb-world-solo-guide-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const APP_SHELL = './index.html';
